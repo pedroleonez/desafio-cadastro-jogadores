c1b7a69846c797bbf8a4fca3696033f3f7d9c1cf
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-15 22:44:02 -0300

    first commit

---
+++ src/main/java/com/example/desafio_cadastro_jogadores/DesafioCadastroJogadoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_cadastro_jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCadastroJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCadastroJogadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_cadastro_jogadores/DesafioCadastroJogadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_cadastro_jogadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCadastroJogadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
